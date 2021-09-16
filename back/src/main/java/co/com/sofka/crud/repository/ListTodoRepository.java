5e95f53a07d2ce287b0008defd5f0c0a073d172a
Hollman Rojas <hollmanerojas@gmail.com>
2021-09-15 23:21:36 -0500

    Se crear repository ListTodoRepository

---
+++ back/src/main/java/co/com/sofka/crud/repository/ListTodoRepository.java
@@ -0,0 +1,8 @@
+package co.com.sofka.crud.repository;
+
+import co.com.sofka.crud.entities.ListTodo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ListTodoRepository extends CrudRepository<ListTodo, Long> {
+
+}
